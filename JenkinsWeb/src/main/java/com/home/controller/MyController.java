ec7dabb21350a90a4200d1ce1fd2b682e6de03b7
aavisha1010 <aakashtare1010@outlook.com>
2024-07-20 22:30:27 +0530

    first changes

--- JenkinsWeb/src/main/java/com/home/controller/MyController.java
+++ JenkinsWeb/src/main/java/com/home/controller/MyController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class MyController {
 	
 	@GetMapping("/")
-	public String greeting(@RequestParam(name="name", required=false, defaultValue="Aakash") String name, Model model) {
+	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
 		model.addAttribute("name", name);
 		return "index";
 	}
