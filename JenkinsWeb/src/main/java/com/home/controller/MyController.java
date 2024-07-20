1f929360064192fcc6677fd0649f485307159e71
aavisha1010 <aakashtare1010@outlook.com>
2024-07-20 22:18:06 +0530

    first commit

---
+++ JenkinsWeb/src/main/java/com/home/controller/MyController.java
@@ -0,0 +1,18 @@
+package com.home.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class MyController {
+	
+	@GetMapping("/")
+	public String greeting(@RequestParam(name="name", required=false, defaultValue="Aakash") String name, Model model) {
+		model.addAttribute("name", name);
+		return "index";
+	}
+
+}
